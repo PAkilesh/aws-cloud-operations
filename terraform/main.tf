44100cc59c28902890d08e613047dde1899563f8
P Akhilesh <palledakhilesh@gmail.com>
2026-08-13 17:39:44 +0530

    Configure Terraform S3 remote backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket       = "akhilesh-terraform-state-2026-653858193267"
+    key          = "aws-cloud-operations/terraform.tfstate"
+    region       = "ap-south-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -13,4 +21,10 @@ provider "aws" {
 
 resource "aws_s3_bucket" "cloud_operations" {
   bucket = var.bucket_name
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.environment
+    ManagedBy   = "Terraform"
+  }
 }
